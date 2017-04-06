75042cf7002a23ca1a9121c08e7306d8d2ff8997
Mrinal Mondal <mrinalm@cybage.com>
2017-04-06 10:57:01 +0530

    replace id by class

--- app/directive/weather-directive.js
+++ app/directive/weather-directive.js
@@ -3,7 +3,7 @@ app.directive("weatherWidget",function(){
 
       return {
                restrict:'EA',
-               template:'<div id="widget"><div id="location">{{loc}}</div><div id="weather"><span id="temparature">{{temp}}</span><span class="deg">&deg;</span><img ng-src="{{img}}"  class="weather-img" ><span id="condition">{{con}}</span> </div><ul id="forcasts"><li ng-repeat="forcast in forcasts"><span>{{forcast.day}} <br> {{forcast.high}}&deg;/{{forcast.low}} </span></li></ul><div class="clear"></div></div> '
+               template:'<div class="widget"><div class="location">{{loc}}</div><div class="weather"><span class="temparature">{{temp}}</span><span class="deg">&deg;</span><img ng-src="{{img}}"  class="weather-img" ><span class="condition">{{con}}</span> </div><ul class="forcasts"><li ng-repeat="forcast in forcasts"><span>{{forcast.day}} <br> {{forcast.high}}&deg;/{{forcast.low}} </span></li></ul><div class="clear"></div></div> '
                
       }
 
